71bed222f6fa14798c410cc4961abbd003095c2c
nedbog <nedbog@gmail.com>
2018-09-03 21:06:09 +0300

    improve the look and feel of the home page

--- GigHub/GigHub/Controllers/HomeController.cs
+++ GigHub/GigHub/Controllers/HomeController.cs
@@ -21,6 +21,7 @@ namespace GigHub.Controllers
         {
             var upComingGigs = _context.Gigs
                 .Include(g => g.Artist)
+                .Include(g => g.Genre)
                 .Where(g => g.DateTime > DateTime.Now);
 
             return View(upComingGigs);
